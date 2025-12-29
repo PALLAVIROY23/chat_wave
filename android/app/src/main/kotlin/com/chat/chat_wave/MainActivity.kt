588406082f201fbd190099fc8cea28ed71b5052d
Roy <royp05774@gmail.com>
2025-12-30 00:32:47 +0530

    firebase setup

---
+++ android/app/src/main/kotlin/com/chat/chat_wave/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.chat.chat_wave
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+import '../modules/home/binding.dart';
+import '../modules/home/view.dart';
+import '../modules/splash/binding.dart';
+import '../modules/splash/view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => const SplashView(),
+      binding: SplashBinding(),
+    ),
+  ];
+}
